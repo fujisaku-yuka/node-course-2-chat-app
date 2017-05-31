e65a0ba21aaffdc1b9c3bad13bed114a325e73ab
Yuka <fujisaku@gmail.com>
2017-05-31 16:15:01 +0700

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+function scrollToBottom() {
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -19,7 +34,9 @@ socket.on('newMessage', function (message) {
     createdAt: formattedTime
   });
 
-  jQuery('#message').append(html);
+  jQuery('#messages').append(html);
+
+  scrollToBottom();
 
   // var li = jQuery('<li></li>');
   // li.text(`${message.from} ${formattedTime}: ${message.text}`)
@@ -38,7 +55,9 @@ socket.on('newLocationMessage', function (message) {
     createdAt: formattedTime
   });
 
-  jQuery('#message').append(html);
+  jQuery('#messages').append(html);
+
+  scrollToBottom();
 
   // var li = jQuery('<li></li>');
   // var a = jQuery('<a target="_blank">My current location</a>')
